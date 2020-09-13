e23b213578a762100a3c0ea6a16893e252949a23
msanvarov <msalanvarov@gmail.com>
2020-09-12 23:53:37 -0400

    :recycle: proper header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,25 +1,129 @@
-import React, { ReactNode } from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEllipsisV, faAward } from '@fortawesome/free-solid-svg-icons';
+import * as menuActions from 'store/menu/actions';
 
-import { Link } from 'components/link/Link';
+import './Header.scss';
+import LogoMDPNG from 'assets/logo-md.png';
+import { RootAppState } from 'store';
+import SideMenuModal from './side-menu-modal/SideMenuModal';
 
-import StarterLogo from 'assets/svg/starter-logo.svg';
+const Header = () => {
+  const dispatch = useDispatch();
+  // const movies = useSelector((state) => state.nominatedMovies);
+  const collapsed = useSelector((state: RootAppState) => state.menu.collapsedState);
 
-import s from './Header.scss';
+  const toggleNavbar = () => {
+    dispatch(menuActions.collapsedState());
+  };
 
-interface HeaderProps {
-  children: ReactNode;
-}
+  useEffect(() => {
+    const elementId = document.getElementById('navbar');
+    document.addEventListener('scroll', () => {
+      if (window.scrollY > 170) {
+        elementId?.classList.add('is-sticky');
+      } else {
+        elementId?.classList.remove('is-sticky');
+      }
+    });
+    window.scrollTo(0, 0);
+  });
 
-export const Header = ({ children }: HeaderProps) => (
-  <header className={s.header}>
-    <div className={s.header__container}>
-      <div className={s.header__content}>
-        <Link to="/" className={s.header__logo}>
-          <StarterLogo className={s.header__logoSvg} />
-        </Link>
+  // side-menu toggler
+  const toggleSideMenuModal = () => {
+    dispatch(menuActions.sideMenuModalState());
+  };
 
-        <div className={s.header__navigation}>{children}</div>
+  const classOne = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
+  const classTwo = collapsed
+    ? 'navbar-toggler navbar-toggler-right collapsed'
+    : 'navbar-toggler navbar-toggler-right';
+
+  return (
+    <header>
+      <div id="navbar" className="navbar-area">
+        <div className="main-nav">
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg">
+              <NavLink to="/" onClick={toggleNavbar} className="navbar-brand">
+                <img src={LogoMDPNG} alt="logo" />
+              </NavLink>
+              <button
+                onClick={toggleNavbar}
+                className={classTwo}
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <FontAwesomeIcon icon={faEllipsisV} />
+              </button>
+              <div className={classOne} id="navbarSupportedContent">
+                <ul className="navbar-nav m-auto">
+                  <li className="nav-item">
+                    <NavLink
+                      to="/"
+                      onClick={toggleNavbar}
+                      className="nav-link"
+                      activeClassName="active"
+                    >
+                      Landing
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/movies"
+                      onClick={toggleNavbar}
+                      className="nav-link"
+                      activeClassName="active"
+                    >
+                      Find Movies
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      to="/nominations"
+                      onClick={toggleNavbar}
+                      className="nav-link"
+                      activeClassName="active"
+                    >
+                      Nominations
+                    </NavLink>
+                  </li>
+                </ul>
+                <div className="others-option">
+                  <div className="cart-icon">
+                    <NavLink to="/nominations">
+                      <FontAwesomeIcon icon={faAward} size="lg" />
+                      <span>{0}</span>
+                    </NavLink>
+                  </div>
+
+                  <div className="menu-icon">
+                    <a
+                      className="burger-menu"
+                      href="/"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        toggleSideMenuModal();
+                      }}
+                    >
+                      <FontAwesomeIcon icon={faEllipsisV} size="lg" />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
       </div>
-    </div>
-  </header>
-);
+      <SideMenuModal />
+    </header>
+  );
+};
+
+export default Header;
